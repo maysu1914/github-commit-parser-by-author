391949bf3946e70d6dac6957f7fc0d0804ccbd08
maysu1914 <mustafa.aysu@hotmail.com>
2021-05-19 00:35:49 +0300

    initial

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # github-commit-parser-by-author
-JavaScript code for parsing commit messages by author from GitHub commits page by using browser code injector
+JavaScript code for parsing commit messages by an author from GitHub commits page by using browser code injector
+
+
+ - Download a code injector extension to your browser (preferred: https://github.com/Lor-Saba/Code-Injector)
+ - Change author_name value to your GitHub username
+ - Import the code to the extension
+ - Refresh the page or Inject code manually
+ - You can see your commits in the console of the browser
---
+++ code.js
@@ -0,0 +1,33 @@
+author_name = 'author'
+
+timeline_bodies = document.getElementsByClassName('TimelineItem-body');
+
+commit_list = [];
+for (let timeline of timeline_bodies) {
+    commit_bodies = timeline.querySelectorAll("div.flex-auto.min-width-0");
+    for (commit_body of commit_bodies) {
+        commit = commit_body.querySelectorAll('p.mb-1, div.Details-content--hidden');
+        author = commit_body.querySelector('a.commit-author');
+        if (getText(author) == author_name) {
+            commit_message = getText(commit);
+            // skip merge commits
+            if (!commit_message.includes('Merge remote-tracking') && !commit_message.includes('Merge branch')) {
+                commit_list.push(commit_message.split('\n').reverse().join('\n'))
+            }
+        }
+    }
+}
+
+console.log(commit_list.reverse().join('\n'))
+
+function getText(element) {
+    if (NodeList.prototype.isPrototypeOf(element)) {
+        text = '';
+        for (el of element) {
+            text += el.textContent.replace('↵', '').replace('…', '').replace('…', '').trim();
+        }
+        return text.split('\n').join('|').replace('||', '|').split('|').join('\n')
+    } else {
+        return element.textContent;
+    }
+}
